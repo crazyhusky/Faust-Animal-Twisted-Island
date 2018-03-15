cda54e007995411d3bb2039a5ed8b16f07f21ab9
noah the goodra <peterpan0413@live.com>
2018-03-15 17:00:26 -0500

    add a mutation class for single body parts
    Signed-off-by: noah the goodra <peterpan0413@live.com>

---
+++ FATI-beta/src/MutationClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FATI_beta.src
+{
+    public class MutationClass : MainGameObject
+    {
+        protected string TransformationText;
+        protected float Percentage;
+        protected string BodyPart;
+        protected string NewBodyPartDescription;
+        public MutationClass()
+        {
+        }
+    }
+}
