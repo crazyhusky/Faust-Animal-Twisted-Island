92f95151596cb32662745784ac0137a7506b9c6c
noah the goodra <peterpan0413@live.com>
2018-03-15 11:46:28 -0500

    make the test form load

--- FATI-beta/Program.cs
+++ FATI-beta/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FATI_beta
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            RunWindow();
+        }
+        static void RunWindow() {
+            Application.EnableVisualStyles();
+            Application.Run(new TestFormX());
         }
     }
 }
