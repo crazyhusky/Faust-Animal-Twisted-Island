325654d3d3c84cf3135bbe04436451478799672a
noah the goodra <peterpan0413@live.com>
2018-03-16 17:42:46 -0500

    add the mount code

--- FATI-beta/Program.cs
+++ FATI-beta/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +10,76 @@ namespace FATI_beta
 {
     class Program
     {
+        protected static PhysFS filesystemLink;
         [STAThread]
         static void Main(string[] args)
         {
+            //FileSystemTests();
+            GameInit();
+            //RunWindow();
+            Console.WriteLine("\n");
+            Console.WriteLine(string.Join("\n",filesystemLink.EnumerateFiles("/")));
+            GameDeInit();
+        }
+
+        private static void FileSystemTests()
+        {
             //Filesystem Test
-            using (var pfs = new PhysFS("")) {
-                pfs.Mount(System.Environment.CurrentDirectory, "",true);
-                using (var reader = new System.IO.StreamReader(pfs.OpenRead("/helloworld.txt")))
+            using (var pfs = new PhysFS(""))
+            {
+                pfs.Mount(System.Environment.CurrentDirectory, "", true);
+                using (var reader = new StreamReader(pfs.OpenRead("/helloworld.txt")))
                 {
                     var contents = reader.ReadToEnd();
                     Console.WriteLine(contents);
                 }
             }
-
-            //RunWindow();
         }
-        static void RunWindow() {
+
+        static void GameInit()
+        {
+            filesystemLink = new PhysFS("");
+            SetupFileSystems();
+        }
+
+        static void GameDeInit()
+        {
+            filesystemLink.Deinit();
+        }
+
+        static void SetupFileSystems()
+        {
+            var seporator = filesystemLink.GetDirSeparator();
+            var BaseDataDir = Environment.CurrentDirectory + seporator + "GameData";
+            var UnPackedData = BaseDataDir + seporator + "Unpacked";
+            var Packed = BaseDataDir + seporator + "Packed";
+            var saveData = BaseDataDir + seporator + "SaveData";
+            filesystemLink.PermitSymbolicLinks(true);
+            Directory.CreateDirectory(saveData);
+            filesystemLink.SetWriteDir(saveData);
+            filesystemLink.Mount(UnPackedData, "", true);
+            SetupPackedData(Packed);
+
+        }
+
+        static void SetupPackedData(string baseDir)
+        {
+            //get all files in alphabetic order
+            var sorted = Directory.GetFiles(baseDir).OrderBy(f => f);
+            foreach (var file in sorted)
+            {
+                try
+                {
+                    filesystemLink.Mount(file, "", false);
+                }
+                catch (PhysFS.PhysFSException e)
+                {
+                    //Console.WriteLine(e);
+                }
+            }
+        }
+        static void RunWindow()
+        {
             Application.EnableVisualStyles();
             Application.Run(new TestFormX());
         }
