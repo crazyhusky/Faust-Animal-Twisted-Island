80bec049dc86f925cf6613cd514ee902177d7228
noah the goodra <peterpan0413@live.com>
2018-03-22 17:49:17 -0500

    rename a few things
    Signed-off-by: noah the goodra <peterpan0413@live.com>

--- FATI-beta/src/Mechanics/JsonMapping.cs
+++ FATI-beta/src/Mechanics/JsonMapping.cs
@@ -4,10 +4,13 @@ namespace FATI_beta.Mechanics
 {
     public class JsonMapping
     {
-        protected Dictionary<string,NPCClass> NPCLists;
+        protected Dictionary<string,NPCClass> NPCList;
         protected Dictionary<string,Location> Locations;
         protected Dictionary<string,ItemClass> Items;
         protected Dictionary<string,TransformationBook> TransformationBooks;
         protected Dictionary<string,TransformationLibrary> TransformationLibraries;
+        protected Dictionary<string,EnemyClass> Enemies;
+        protected Dictionary<string,EventClass> Events;
+        protected PlayerClass BasePlayer;
     }
 }
