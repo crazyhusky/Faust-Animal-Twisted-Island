f0b5e548422075f594b96809cb19a991c4af5af6
noah the goodra <peterpan0413@live.com>
2018-03-22 16:54:33 -0500

    Start adding Dictionaries
    Signed-off-by: noah the goodra <peterpan0413@live.com>

--- FATI-beta/src/Mechanics/JsonMapping.cs
+++ FATI-beta/src/Mechanics/JsonMapping.cs
@@ -1,7 +1,13 @@
-﻿namespace FATI_beta.Mechanics
+﻿using System.Collections.Generic;
+using FATI_beta.JsonClasses;
+namespace FATI_beta.Mechanics
 {
     public class JsonMapping
     {
-        
+        protected Dictionary<string,NPCClass> NPCLists;
+        protected Dictionary<string,Location> Locations;
+        protected Dictionary<string,ItemClass> Items;
+        protected Dictionary<string,TransformationBook> TransformationBooks;
+        protected Dictionary<string,TransformationLibrary> TransformationLibraries;
     }
 }
