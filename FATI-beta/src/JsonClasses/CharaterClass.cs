b998f35eaa02904778e868626c56e4b19942f6c7
noah the goodra <peterpan0413@live.com>
2018-03-22 17:05:29 -0500

    Add more properties to CharaterClass
    Signed-off-by: noah the goodra <peterpan0413@live.com>

--- FATI-beta/src/JsonClasses/CharaterClass.cs
+++ FATI-beta/src/JsonClasses/CharaterClass.cs
@@ -1,10 +1,14 @@
-﻿namespace FATI_beta.JsonClasses
+﻿using System.Collections.Generic;
+
+namespace FATI_beta.JsonClasses
 {
     public class CharaterClass : MainGameObject
     {
         protected float Health;
         protected float Lust;
         protected int Coins;
+        protected List<BodyPartBaseClass> BodyParts;
+        protected List<ItemClass> Inventory;
         public CharaterClass()
         {
         }
