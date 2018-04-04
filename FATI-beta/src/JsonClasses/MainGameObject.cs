a7e3e204f626dbe413e86f8b92f7ce13b96a594a
noah the goodra <peterpan0413@live.com>
2018-04-03 21:42:51 -0500

    convert to interpolation
    Signed-off-by: noah the goodra <peterpan0413@live.com>

--- FATI-beta/src/JsonClasses/MainGameObject.cs
+++ FATI-beta/src/JsonClasses/MainGameObject.cs
@@ -8,7 +8,7 @@
         {
         }
         public override string ToString() {
-            return Name +":"+ Description +":"+ GetType().Name;
+            return $"{Name}:{Description}:{GetType().Name}";
         }
     }
 }
