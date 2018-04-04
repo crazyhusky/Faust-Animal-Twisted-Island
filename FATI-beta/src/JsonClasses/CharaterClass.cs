b479fdcea4aac3fd4ddcba3d66b8826bc3253a9a
noah the goodra <peterpan0413@live.com>
2018-04-03 21:47:30 -0500

    give locations an inventory and add movement funct
    Signed-off-by: noah the goodra <peterpan0413@live.com>

--- FATI-beta/src/JsonClasses/CharaterClass.cs
+++ FATI-beta/src/JsonClasses/CharaterClass.cs
@@ -9,8 +9,14 @@ namespace FATI_beta.JsonClasses
         protected int Coins;
         protected List<BodyPartBaseClass> BodyParts;
         protected List<ItemClass> Inventory;
+        protected Location CurrentLocation;
         public CharaterClass()
         {
+        }
+
+        public void MoveToLocation(Location newlocation)
+        {
+            CurrentLocation = newlocation;
         }
     }
 }
--- FATI-beta/src/JsonClasses/Location.cs
+++ FATI-beta/src/JsonClasses/Location.cs
@@ -1,9 +1,12 @@
-﻿namespace FATI_beta.JsonClasses
+﻿using System.Collections.Generic;
+
+namespace FATI_beta.JsonClasses
 {
     public class Location: MainGameObject
     {
         protected LocationConnections[] Connections;
         protected int UnlockLevel;
+        protected List<ItemClass> Inventory;
     }
 
     public class LocationConnections : MainGameObject
