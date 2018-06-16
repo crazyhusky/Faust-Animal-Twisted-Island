965dc6ac2cd83ac1e4d4b53c203ed503c714780f
noah the goodra <peterpan0413@live.com>
2018-06-16 13:18:58 -0500

    make UI interface

---
+++ FATI-beta/src/UI/UIConnectionInterface.cs
@@ -0,0 +1,10 @@
+﻿namespace FATIbeta.src.UI
+{
+    public interface UIConnectionInterface
+    {
+        void ClearPlayerDescriptionScreen();
+        void ClearWorldScreen();
+        void UpdatePlayerDescription(string PlayerText);
+        void UpdateWorldDescription(string WorldDescription);
+    }
+}
