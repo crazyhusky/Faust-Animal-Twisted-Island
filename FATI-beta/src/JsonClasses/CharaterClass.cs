11d44892ffa55a397389a90e6922d9cf66cdb4fd
noah the goodra <peterpan0413@live.com>
2018-06-17 17:26:29 -0500

    make properties public

--- FATI-beta/src/JsonClasses/CharaterClass.cs
+++ FATI-beta/src/JsonClasses/CharaterClass.cs
@@ -4,14 +4,30 @@ namespace FATIbeta.JsonClasses
 {
     public class CharaterClass : MainGameObject
     {
-        protected float Health;
-        protected float MaxHealth;
-        protected float Lust;
-        protected int Coins;
-        protected int Level;
-        protected List<BodyPartBaseClass> BodyParts;
-        protected List<ItemClass> Inventory;
-        protected Location CurrentLocation;
+        private float health;
+        private float maxHealth;
+        private float lust;
+        private float maxlust;
+        private float fatique;
+        private float maxfatique;
+        private int coins;
+        private int level;
+        private List<BodyPartBaseClass> bodyParts;
+        private List<ItemClass> inventory;
+        private Location currentLocation;
+
+        public float Health { get => health; set => health = value; }
+        public float MaxHealth { get => maxHealth; set => maxHealth = value; }
+        public float MaxLust { get => maxlust; set => maxlust = value; }
+        public float Lust { get => lust; set => lust = value; }
+        public float MaxFatique { get => maxfatique; set => maxfatique = value; }
+        public float Fatique { get => fatique; set => fatique = value; }
+        public int Coins { get => coins; set => coins = value; }
+        public int Level { get => level; set => level = value; }
+        public List<BodyPartBaseClass> BodyParts { get => bodyParts; set => bodyParts = value; }
+        public List<ItemClass> Inventory { get => inventory; set => inventory = value; }
+        public Location CurrentLocation { get => currentLocation; set => currentLocation = value; }
+
         public CharaterClass()
         {
         }
@@ -19,11 +35,6 @@ namespace FATIbeta.JsonClasses
         public void MoveToLocation(Location newlocation)
         {
             CurrentLocation = newlocation;
-        }
-
-        public int GetLevel()
-        {
-            return Level;
         }
     }
 }
