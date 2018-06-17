4b46d39ea03dcfe076de273ab4789cb38b57d847
noah the goodra <peterpan0413@live.com>
2018-06-17 17:26:04 -0500

    begin writing the linking code for the interface

---
+++ FATI-beta/src/UI/winformLink.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FATIbeta.JsonClasses;
+
+namespace FATIbeta.src.UI
+{
+    public class winformLink : UIConnectionInterface
+    {
+        private TestFormX winformInstance;
+        public winformLink( TestFormX formInstance)
+        {
+            winformInstance = formInstance;
+        }
+
+        public void ClearPlayerDescriptionScreen()
+        {
+            winformInstance.SetPlayerText("");
+        }
+
+        public void ClearWorldScreen()
+        {
+            winformInstance.SetworldText("");
+        }
+
+        public void UpdatePlayerData(PlayerClass PlayerClassData)
+        {
+            winformInstance.SetPlayerText(PlayerClassData.Description);
+            winformInstance.UpdatePlayerHealth(PlayerClassData.Health, PlayerClassData.MaxHealth);
+            winformInstance.UpdatePlayerLust(PlayerClassData.Lust,PlayerClassData.MaxLust);
+            winformInstance.UpdatePlayerFatigue(PlayerClassData.Fatique, PlayerClassData.MaxFatique);
+        }
+
+        public void UpdateWorldDescription(string WorldDescription)
+        {
+            winformInstance.SetworldText(WorldDescription);
+        }
+    }
+}
