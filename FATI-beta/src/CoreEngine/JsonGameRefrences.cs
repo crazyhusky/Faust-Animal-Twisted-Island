b751f740b56b9b3b0e2a2599b4812c2db7d83552
noah the goodra <peterpan0413@live.com>
2018-06-18 18:39:55 -0500

    add code for the linking bits

---
+++ FATI-beta/src/CoreEngine/JsonGameRefrences.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FATIbeta.CoreEngine;
+using FATIbeta.JsonClasses;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace FATIbeta.src.CoreEngine
+{
+    class JsonGameRefrences
+    {
+        public class LocationRefrences : JsonConverter<Location>
+        {
+
+            public override void WriteJson(JsonWriter writer, Location value, JsonSerializer serializer)
+            {
+                writer.WriteValue(value.Name);
+            }
+
+            public override Location ReadJson(JsonReader reader, Type objectType, Location existingValue, bool hasExistingValue,
+                JsonSerializer serializer)
+            {
+                return Program.MainGame.GetJsonMappingInstance().GetLocation(reader.Value as string);
+            }
+        }
+
+
+    }
+}
