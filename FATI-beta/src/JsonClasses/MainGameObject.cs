82a739579fa0b6583ab18240935a067e0863f505
noah the goodra <peterpan0413@live.com>
2018-06-18 21:35:24 -0500

    main game object clone

--- FATI-beta/src/JsonClasses/MainGameObject.cs
+++ FATI-beta/src/JsonClasses/MainGameObject.cs
@@ -1,21 +1,30 @@
-﻿using Jint.Parser.Ast;
+﻿using System.CodeDom;
+using System.ComponentModel;
+using Jint.Parser.Ast;
 
 namespace FATIbeta.JsonClasses
 {
     public class MainGameObject
     {
+        [DefaultValue("Thing")]
         public string Name { get; set; }
+        [DefaultValue("Thing Description")]
         public string Description { get; set; }
         public MainGameObject()
         {
         }
         public override string ToString() {
-            return $"{Name}:{Description}:{GetType().Name}";
+            return $"{Name}:{Description}";
         }
 
         public void UpdateObject()
         {
 
+        }
+
+        public MainGameObject GetShallowCopy()
+        {
+            return this.MemberwiseClone() as MainGameObject;
         }
     }
 }
