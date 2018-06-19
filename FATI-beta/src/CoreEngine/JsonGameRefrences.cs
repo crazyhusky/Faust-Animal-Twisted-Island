212f6fc50d96f4a3034742c8ef43f47c7737a20f
noah the goodra <peterpan0413@live.com>
2018-06-18 21:31:52 -0500

    add special json loaders

--- FATI-beta/src/CoreEngine/JsonGameRefrences.cs
+++ FATI-beta/src/CoreEngine/JsonGameRefrences.cs
@@ -28,7 +28,27 @@ namespace FATIbeta.src.CoreEngine
                 return Program.MainGame.GetJsonMappingInstance().GetLocation(reader.Value as string);
             }
         }
+        public class BodyPartRefrences : JsonConverter<List<BodyPartBaseClass>>
+        {
 
+            public override void WriteJson(JsonWriter writer, List<BodyPartBaseClass> value, JsonSerializer serializer)
+            {
+                writer.WriteValue(value);
+            }
+
+            public override List<BodyPartBaseClass> ReadJson(JsonReader reader, Type objectType, List<BodyPartBaseClass> existingValue, bool hasExistingValue,
+                JsonSerializer serializer)
+            {
+                var parts = JArray.Load(reader).ToObject<List<string>>();
+                var newlist = new List<BodyPartBaseClass>();
+                foreach (var bodypart in parts)
+                {
+                    var output = Program.MainGame.GetJsonMappingInstance().GetBodyPart(bodypart).GetShallowCopy();
+                    newlist.Add(output);
+                }
+                return newlist;
+            }
+        }
 
     }
 }
