c9d4b3d4a34b9213f890d2807e7e15ea8b65eaed
noah the goodra <peterpan0413@live.com>
2018-06-18 21:32:47 -0500

    update options for asset loader

--- FATI-beta/src/CoreEngine/AssetLoading.cs
+++ FATI-beta/src/CoreEngine/AssetLoading.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.IO;
 using FATIbeta.JsonClasses;
+using FATIbeta.src.CoreEngine;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using SharpPhysFS;
 
 namespace FATIbeta.CoreEngine
 {
     public class AssetLoading
     {
+        private JsonMapping MapsInstance;
+        public AssetLoading(JsonMapping maps)
+        {
+            MapsInstance = maps;
+        }
+
         public void LoadResource<T>(String filenameToEnum, PhysFS physFSInstance, Dictionary<string, T> MapDictionary) where T: MainGameObject, new()
         {
             var FileList = physFSInstance.EnumerateFiles(filenameToEnum);
@@ -16,17 +24,41 @@ namespace FATIbeta.CoreEngine
             {
                 var filepath = filenameToEnum + "/" + file;
                 var locationData = DeserializeJsonFile<T>(physFSInstance, filepath);
-                MapDictionary.Add(locationData.Name, locationData);
+                MapDictionary.Add(locationData.Name.Replace(" ","-"), locationData);
             }
+        }
+        public List<T> LoadSingleResource<T>(String filenameToEnum, PhysFS physFSInstance) where T : MainGameObject, new()
+        {
+            var dataset = new List<T>();
+            var FileList = physFSInstance.EnumerateFiles(filenameToEnum);
+            foreach (var file in FileList)
+            {
+                var filepath = filenameToEnum + "/" + file;
+                var locationData = DeserializeJsonFile<T>(physFSInstance, filepath);
+                dataset.Add(locationData);
+            }
+
+            return dataset;
         }
         public T DeserializeJsonFileData<T>(Stream s)
         {
-            var deserializeOptions = new JsonSerializerSettings {DefaultValueHandling = DefaultValueHandling.Populate};
+            ITraceWriter traceWriter = new MemoryTraceWriter();
+            var deserializeOptions = new JsonSerializerSettings
+            {
+                TraceWriter = traceWriter,
+                DefaultValueHandling = DefaultValueHandling.Populate,
+                NullValueHandling = NullValueHandling.Include,
+                PreserveReferencesHandling = PreserveReferencesHandling.All,
+                
+            };
             using (var reader = new StreamReader(s))
             using (var jsonReader = new JsonTextReader(reader))
             {
                 JsonSerializer ser = JsonSerializer.CreateDefault(deserializeOptions);
-                return ser.Deserialize<T>(jsonReader);
+                var output = ser.Deserialize<T>(jsonReader);
+                Console.WriteLine(traceWriter);
+                return output;
+
             }
         }
         public T DeserializeJsonFile<T>(PhysFS physFSInstance, string filepath) where T : MainGameObject, new()
