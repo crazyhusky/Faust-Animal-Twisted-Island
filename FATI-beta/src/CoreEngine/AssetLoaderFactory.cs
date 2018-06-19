1ebd42eb66e53eb9431e9c0ae1b7f71508e2338a
noah the goodra <peterpan0413@live.com>
2018-06-18 21:32:14 -0500

    got player json to load

--- FATI-beta/src/CoreEngine/AssetLoaderFactory.cs
+++ FATI-beta/src/CoreEngine/AssetLoaderFactory.cs
@@ -1,4 +1,6 @@
-﻿using SharpPhysFS;
+﻿using System.Linq;
+using FATIbeta.JsonClasses;
+using SharpPhysFS;
 
 namespace FATIbeta.CoreEngine
 {
@@ -6,17 +8,17 @@ namespace FATIbeta.CoreEngine
     {
         public static void LoadFiles(PhysFS physFSInstance, JsonMapping Maps)
         {
-
-            new AssetLoading().LoadResource("attacks", physFSInstance, Maps.Attacks);
-            new AssetLoading().LoadResource("bodyparts", physFSInstance, Maps.BodyParts);
-            new AssetLoading().LoadResource("events", physFSInstance, Maps.Events);
-            new AssetLoading().LoadResource("items", physFSInstance, Maps.Items);
-            new AssetLoading().LoadResource("npcs", physFSInstance, Maps.NPCList);
-            new AssetLoading().LoadResource("locations", physFSInstance, Maps.Locations);
-            new AssetLoading().LoadResource("enemies", physFSInstance, Maps.Enemies);
-            new AssetLoading().LoadResource("tfbooks", physFSInstance, Maps.TransformationBooks);
-            new AssetLoading().LoadResource("tflibrary", physFSInstance, Maps.TransformationLibraries);
-            //new AssetLoading().LoadResource("player", physFSInstance, Maps.BasePlayer);
+            var assetLoading = new AssetLoading(Maps);
+            assetLoading.LoadResource("attacks", physFSInstance, Maps.Attacks);
+            assetLoading.LoadResource("bodyparts", physFSInstance, Maps.BodyParts);
+            assetLoading.LoadResource("events", physFSInstance, Maps.Events);
+            assetLoading.LoadResource("items", physFSInstance, Maps.Items);
+            assetLoading.LoadResource("npcs", physFSInstance, Maps.NPCList);
+            assetLoading.LoadResource("locations", physFSInstance, Maps.Locations);
+            assetLoading.LoadResource("enemies", physFSInstance, Maps.Enemies);
+            assetLoading.LoadResource("tfbooks", physFSInstance, Maps.TransformationBooks);
+            assetLoading.LoadResource("tflibrary", physFSInstance, Maps.TransformationLibraries);
+            Maps.BasePlayer = assetLoading.LoadSingleResource<PlayerClass>("player", physFSInstance).Last();
 
         }
     }
